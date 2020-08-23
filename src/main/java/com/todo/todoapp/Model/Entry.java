19c312d20bb4a4bb90a9ed7d6453dffc5e195839
Greddyy <code.enthusiasm@gmail.com>
2020-08-23 21:49:52 +0300

    fixed naming conventions

--- src/main/java/com/todo/todoapp/Model/Entry.java
+++ src/main/java/com/todo/todoapp/Model/Entry.java
@@ -2,17 +2,18 @@ package com.todo.todoapp.Model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Entries")
+@Table(name = "entries")
 public class Entry {
 
     @Id
     private long id;
-    private String entryName;
-    private String entryDescription; //content
+    private String name;
+    private String description; //content
     private boolean completed;
-    private Timestamp dateEntered;
+    private LocalDateTime localDateTime;
     @ManyToOne
     @JoinColumn(name="User", insertable = false, updatable = false)
     private User user;
--- src/main/java/com/todo/todoapp/Model/User.java
+++ src/main/java/com/todo/todoapp/Model/User.java
@@ -4,15 +4,14 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String Username;
-    private String Password;
-    private String ConfirmPassword;
-    private boolean isAdmin;
-    @OneToMany(mappedBy = "Entry")
+    private String username;
+    private String password;
+    private boolean admin;
+    @OneToMany(mappedBy = "entries")
     private List<Entry> entryList;
 }
