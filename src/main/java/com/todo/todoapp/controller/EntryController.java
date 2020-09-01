b1bb0d1dc8d6fee0debedfc580f4d56d0cb3b4f4
Greddyy <code.enthusiasm@gmail.com>
2020-09-01 14:14:16 +0300

    added entry services and entry endpoints

--- src/main/java/com/todo/todoapp/controller/EntryController.java
+++ src/main/java/com/todo/todoapp/controller/EntryController.java
@@ -1,22 +1,29 @@
 package com.todo.todoapp.controller;
 
 import com.todo.todoapp.model.Entry;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.todo.todoapp.services.EntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EntryController {
 
+    @Autowired
+    EntryService entryService;
 
+    @PostMapping("/entry")
     public void saveEntry(@RequestBody Entry entry){
-
+        entryService.saveEntry(entry);
     }
-
+    @GetMapping("/getEntries")
     public List<Entry> getAllEntries(){
-
-        return null;
+        return entryService.getEntries();
+    }
+    @GetMapping
+    public Optional<Entry> getEntryById(@PathVariable long id){
+        return entryService.getEntryById(id);
     }
 }
--- src/main/java/com/todo/todoapp/services/EntryService.java
+++ src/main/java/com/todo/todoapp/services/EntryService.java
@@ -1,4 +1,32 @@
 package com.todo.todoapp.services;
 
+
+import com.todo.todoapp.model.Entry;
+import com.todo.todoapp.repository.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EntryService {
+
+    @Autowired
+    private EntryRepository entryRepository;
+
+    public List<Entry> getEntries(){
+        return (List<Entry>) entryRepository.findAll();
+    }
+
+    //why this converts to optional?
+    public Optional<Entry> getEntryById(long id){
+        return entryRepository.findById(id);
+    }
+
+    public void saveEntry(Entry entry){
+        entryRepository.save(entry);
+    }
+
+
 }
