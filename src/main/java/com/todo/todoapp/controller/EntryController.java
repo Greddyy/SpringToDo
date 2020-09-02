68a364ab6c8ef17790f9ffd0d74d92ded69dcf37
Greddyy <code.enthusiasm@gmail.com>
2020-09-02 12:54:19 +0300

    fixed entrybyId

--- src/main/java/com/todo/todoapp/controller/EntryController.java
+++ src/main/java/com/todo/todoapp/controller/EntryController.java
@@ -19,21 +19,29 @@ public class EntryController {
     EntryRepository entryRepository;
 
     @PostMapping("/entry")
-    public ResponseEntity saveEntry(@RequestBody Entry entry){
+    public ResponseEntity saveEntry(@RequestBody Entry entry) {
         return new ResponseEntity(entryService.saveEntry(entry), HttpStatus.valueOf(201));
 
 
     }
+
     @GetMapping("/entries")
-    public List<Entry> getAllEntries(){
+    public List<Entry> getAllEntries() {
         return entryService.getEntries();
     }
+
     @GetMapping("/getEntryById/{id}")
-    public ResponseEntity<Entry> getEntryById(@PathVariable long id){
-        Optional<Entry> optionalEntry =  entryRepository.findById(id);
-        Entry entry = optionalEntry.get();
-        return new ResponseEntity<>(entry, HttpStatus.valueOf(404));
+    public ResponseEntity<Entry> getEntryById(@PathVariable long id) {
+        Optional<Entry> optionalEntry = entryRepository.findById(id);
+        if (optionalEntry.isPresent()) {
+            Entry entry = optionalEntry.get();
+            return ResponseEntity.status(HttpStatus.OK).body(entry);
+        } else {
+            return new ResponseEntity<>(HttpStatus.valueOf(404));
+
         }
+    }
+
 }
 
 
